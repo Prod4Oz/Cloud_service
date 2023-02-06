fc1f42a79c5a037fca4e6d07bb50b3ee81c03ff9
Prod4Oz <nn52-ivan@outlook.com>
2023-02-06 20:44:38 +0300

    AuthController finish

--- src/main/java/ru/prod/Cloud_service/controllers/AuthController.java
+++ src/main/java/ru/prod/Cloud_service/controllers/AuthController.java
@@ -1,9 +1,11 @@
 package ru.prod.Cloud_service.controllers;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.prod.Cloud_service.dto.TokenDTO;
+import ru.prod.Cloud_service.dto.UserDTO;
 import ru.prod.Cloud_service.services.AuthorizationService;
 
 @RestController
@@ -14,11 +16,11 @@ public class AuthController {
     private final AuthorizationService authorizationService;
 
     @PostMapping("/login")
-    public void login(){
-
+    public TokenDTO login(@Valid @RequestBody UserDTO userDTO){
+        return authorizationService.login(userDTO);
     }
     @PostMapping("/logout")
-    public void logout(){
-
+    public void logout(@RequestHeader("auth-token") @NotBlank String authToken) {
+        authorizationService.logout(authToken);
     }
 }
