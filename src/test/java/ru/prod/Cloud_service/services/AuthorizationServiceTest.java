2f4648638789f4a8107d9bd528fd49a159e26667
Prod4Oz <nn52-ivan@outlook.com>
2023-02-06 17:12:09 +0300

    AuthorizationServiceTest

--- src/test/java/ru/prod/Cloud_service/services/AuthorizationServiceTest.java
+++ src/test/java/ru/prod/Cloud_service/services/AuthorizationServiceTest.java
@@ -1,30 +1,90 @@
 package ru.prod.Cloud_service.services;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.prod.Cloud_service.Entity_models.User;
+import ru.prod.Cloud_service.dto.UserDTO;
+import ru.prod.Cloud_service.repositories.TokenRepository;
+import ru.prod.Cloud_service.repositories.UserRepository;
+import ru.prod.Cloud_service.utils.JWTUtil;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class AuthorizationServiceTest {
 
-    @BeforeEach
-    void setUp() {
+    public static final String BAD_AUTH_TOKEN = "gaarahzfbzfbzaf";
+    public static final String USER = "user";
+    public static final String BAD_USER = "no_user";
+    public static final String PASSWORD = "user";
+    public static final String BAD_PASSWORD = "password";
+
+
+    private final JWTUtil jwtUtil = createJWTUtilMock();
+
+    private final UserRepository userRepository = createUserRepositoryMock();
+    private final TokenRepository tokenRepository = createTokenRepositoryMock();
+
+
+    private UserRepository createUserRepositoryMock() {
+        final UserRepository userRepository = Mockito.mock(UserRepository.class);
+        when(userRepository.findByUsername(USER)).thenReturn(Optional.of(new User(1L, USER, PASSWORD)));
+        when(userRepository.findByUsername(BAD_USER)).thenReturn(Optional.empty());
+        return userRepository;
     }
 
-    @AfterEach
-    void tearDown() {
+    private TokenRepository createTokenRepositoryMock() {
+        final TokenRepository tokenRepository = Mockito.mock(TokenRepository.class);
+        when(tokenRepository.existsById(jwtUtil.generateToken(USER))).thenReturn(true);
+        when(tokenRepository.existsById(BAD_AUTH_TOKEN)).thenReturn(false);
+        return tokenRepository;
     }
+
+    private JWTUtil createJWTUtilMock() {
+        final JWTUtil jwtUtil = Mockito.mock(JWTUtil.class);
+        return jwtUtil;
+    }
+
 
     @Test
     void login() {
+        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository, jwtUtil);
+        Assertions.assertDoesNotThrow(() -> authorizationService.login(new UserDTO(USER, PASSWORD)));
     }
 
     @Test
     void logout() {
+        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository, jwtUtil);
+        Assertions.assertDoesNotThrow(() -> authorizationService.logout(jwtUtil.generateToken(USER)));
+    }
+
+    @Test
+    void login_userNotFound() {
+        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository, jwtUtil);
+        Assertions.assertThrows(RuntimeException.class, () -> authorizationService.login(new UserDTO(BAD_USER, PASSWORD)));
+    }
+
+
+    @Test
+    void login_incorrectPassword() {
+        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository, jwtUtil);
+        Assertions.assertThrows(RuntimeException.class, () -> authorizationService.login(new UserDTO(USER, BAD_PASSWORD)));
     }
 
     @Test
     void checkToken() {
+        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository, jwtUtil);
+        Assertions.assertDoesNotThrow(() -> authorizationService.checkToken(jwtUtil.generateToken(USER)));
+    }
+
+    @Test
+    void checkToken_failed() {
+        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository, jwtUtil);
+        Assertions.assertThrows(RuntimeException.class, () -> authorizationService.checkToken(BAD_AUTH_TOKEN));
     }
 }
