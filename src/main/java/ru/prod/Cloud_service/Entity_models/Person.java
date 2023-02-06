df797a2d276cd461bcbf81eb6295f53f551b87c5
Prod4Oz <nn52-ivan@outlook.com>
2023-02-06 13:16:10 +0300

    Add  package entity_models Start development authentication

---
+++ src/main/java/ru/prod/Cloud_service/Entity_models/Person.java
@@ -0,0 +1,27 @@
+package ru.prod.Cloud_service.Entity_models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+}
